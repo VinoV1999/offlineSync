f8c02d1aae01a18841a1596fa4af455fed5bee8d
vinoth <vinothkumar281019@gmail.com>
2025-04-05 20:59:52 +0530

    added setup script

--- setup.js
+++ setup.js
@@ -1,5 +1,8 @@
 const { execSync } = require('child_process');
 
 console.log('Installing dependencies...');
+execSync('npm install', { stdio: 'inherit' });
+console.log('Installing Frontend dependencies...');
 execSync('cd frontend && npm install', { stdio: 'inherit' });
+console.log('Installing Backend dependencies...');
 execSync('cd backend && npm install', { stdio: 'inherit' });
